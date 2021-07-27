85288d635ea71d735ff4cf17fe45892630bd5a66
leerom <leerom@163.com>
2021-07-27 14:26:52 +0800

    Update Ripple.js
    change function name

--- Ripple.js
+++ Ripple.js
@@ -2,13 +2,13 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 var scene = viewer.scene;
 
 var matGLSL =
-    'vec3 RadarPing(in vec2 uv, in vec2 center, in float innerTail, \n' +
+    'vec3 RipplePing(in vec2 uv, in vec2 center, in float innerTail, \n' +
     'in float frontierBorder, in float timeResetSeconds, \n' +
-    'in float radarPingSpeed, in float fadeDistance, float t) \n' +
+    'in float RipplePingSpeed, in float fadeDistance, float t) \n' +
     '{ \n' +
     'vec2 diff = center - uv; \n' +
     'float r = length(diff); \n' +
-    'float time = mod(t, timeResetSeconds) * radarPingSpeed; \n' +
+    'float time = mod(t, timeResetSeconds) * RipplePingSpeed; \n' +
     'float circle; \n' +
     'circle += smoothstep(time - innerTail, time, r) * smoothstep(time + frontierBorder, time, r); \n' +
     'circle *= smoothstep(fadeDistance, 0.0, r); \n' +
@@ -22,13 +22,13 @@ var matGLSL =
     'uv += vec2(-1.0, -1.0); \n' +
     'float fadeDistance = 1.8; \n' +
     'float resetTimeSec = 5.; \n' +
-    'float radarPingSpeed = 0.2; \n' +
+    'float RipplePingSpeed = 0.2; \n' +
     'vec2 greenPing = vec2(0.0, 0.0); \n' +
     'vec3 outColor;\n' +
     'float iTime = czm_frameNumber * 0.01; \n' +
-    'outColor += RadarPing(uv, greenPing, 0.08, 0.00025, resetTimeSec, radarPingSpeed, fadeDistance, iTime); \n' +
-    'outColor += RadarPing(uv, greenPing, 0.08, 0.00025, resetTimeSec, radarPingSpeed, fadeDistance, iTime + 1.5); \n' +
-    'outColor += RadarPing(uv, greenPing, 0.08, 0.00025, resetTimeSec, radarPingSpeed, fadeDistance, iTime + 3.0); \n' +
+    'outColor += RipplePing(uv, greenPing, 0.08, 0.00025, resetTimeSec, RipplePingSpeed, fadeDistance, iTime); \n' +
+    'outColor += RipplePing(uv, greenPing, 0.08, 0.00025, resetTimeSec, RipplePingSpeed, fadeDistance, iTime + 1.5); \n' +
+    'outColor += RipplePing(uv, greenPing, 0.08, 0.00025, resetTimeSec, RipplePingSpeed, fadeDistance, iTime + 3.0); \n' +
     'm.diffuse = outColor * color.xyz; \n' +
     'm.alpha = outColor.r; \n' +
     'return m; \n' +
